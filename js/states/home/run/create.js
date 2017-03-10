cbc5e018f7728c6cb25dd23d247b1f9977f7f9de
LucasDenele <lucas.denele@isen-lille.fr>
2017-03-10 14:34:27 +0100

    Placement automatique des sprites

--- js/states/home/run/create.js
+++ js/states/home/run/create.js
@@ -7,13 +7,20 @@ define(['phaser'],function(phaser){
 		function launchCredits(){
 			game.state.start('credits');
 		}
-		console.log('Create Home');
-
+		console.log('Create Home');		
 		game.add.sprite(0,0,'background');
 		game.add.text(250, 50, 'LE TITRE DU JEU', {font: '30px Courier', fill: '#ffffff'});
-		game.add.button(700, 500, 'Optionsbutton', launchOptions, this, 2, 1, 0);
-		game.add.button(0, 500, 'Creditsbutton', launchCredits, this, 2, 1, 0);
+		
+
+		//Affichage du boutton options :
+		this._optionsButton = game.add.button(0, 0, 'Optionsbutton', launchOptions, this, 2, 1, 0);
+		this._optionsButton.centerX = game.width - this._optionsButton.width/2 - 10; // -10 pour éviter qu'il colle à la bordure
+		this._optionsButton.centerY = game.height - this._optionsButton.height/2 - 10;
 	
+		//Affichage du boutton credits :
+		this._creditsButton = game.add.button(0, 0, 'Creditsbutton', launchCredits, this, 2, 1, 0);
+		this._creditsButton.centerX = this._creditsButton.width/2 + 10; // + 10 pour éviter qu'il colle à la bordure
+		this._creditsButton.centerY = game.height - this._creditsButton.height/2 - 10;
 	}
 
 	return create;
