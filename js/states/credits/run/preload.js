49765dc40ae7f19431d53d3845abafc4a7c14bf7
LucasDenele <lucas.denele@isen-lille.fr>
2017-03-17 14:30:15 +0100

    Changement de fond

--- js/states/credits/run/preload.js
+++ js/states/credits/run/preload.js
@@ -4,7 +4,7 @@ define(['phaser'],function(phaser){
 		
 		game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
 
-		game.load.image('background', 'assets/background.gif');
+		game.load.image('background', 'assets/background.jpg');
 		game.load.image('backButton', 'assets/back.png');
 		game.load.image('briquet', 'assets/briquet.jpg');
 		game.load.image('carlier', 'assets/carlier.jpg');
