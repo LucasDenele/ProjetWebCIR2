210afe036561e9ce14413cf8b98126fe5ed1db52
Chewbaku <anthony.carlier@isen-lille.fr>
2017-03-17 16:14:40 +0100

    Version debuggée

--- js/states/options/run/preload.js
+++ js/states/options/run/preload.js
@@ -4,13 +4,8 @@ define(['phaser'],function(phaser){
 		
 		game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
 
-<<<<<<< HEAD
 		game.load.image('background', 'assets/background.jpg');
 		game.load.image('backButton', 'assets/back.png');
-=======
-		game.load.image('background', 'assets/background.gif');
-		game.load.image('backButton', 'assets/buttons/back.png');
->>>>>>> 5dcb7db161d2b9a9289caf09b60f37743ed528c1
 
 	}
 
