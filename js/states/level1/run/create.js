8cc26183895293d2abd4803c2bbb4969192e9051
Chewbaku <anthony.carlier@isen-lille.fr>
2017-05-13 14:44:44 +0200

    MAJ pour tous les items

--- js/states/level1/run/create.js
+++ js/states/level1/run/create.js
@@ -5,11 +5,7 @@ define(['phaser', 'NPC', 'ITEM'], function(phaser, NPC, ITEM){
 		function backToMenu(){
 			game.state.start('home');
 		}
-        
-        function putItemOff(){
-            console.log('test');
-        }
-        
+                
         //Declarations des éléments à afficher
 		game.add.sprite(0,0,'background');
 		game.add.text(10, 10, 'Level 1 :', {font: '30px Courier', fill: '#ffffff'});
@@ -17,7 +13,7 @@ define(['phaser', 'NPC', 'ITEM'], function(phaser, NPC, ITEM){
         
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        //Déclaration Item
+        //Déclaration des Items
         
         this._tv = new Item(game);
         this._tv.setObjectType(game, 1);
@@ -33,14 +29,6 @@ define(['phaser', 'NPC', 'ITEM'], function(phaser, NPC, ITEM){
         this._wash.setObjectType(game, 3);
         this._wash.setY(100);
         game.physics.arcade.enable(this._wash.sprite);
-        
-        //this._tv = game.add.sprite(100, 15, 'tv');
-        //this._tv.scale.setTo(0.25, 0.25);
-        //this._tv.frame = 4;
-        //this._tv.animations.add('on', [0, 1, 2, 3, 3, 2, 1], 3, true);
-        //this._tv.inputEnable = true;
-        this._tv.sprite.events.onInputDown.add(putItemOff, this);
-        
         
         //Déclaration du PNJ
         this._npc = new Npc(game);
